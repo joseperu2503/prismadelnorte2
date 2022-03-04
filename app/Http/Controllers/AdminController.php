8fc755fdd9df9817f7e8b2f75b47714a670939bb
Jose Perez <joseperu2503@gmail.com>
2022-03-04 17:34:47 -0500

    ruta post corregida

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.index');
+        $posts = Post::select('*')
+        ->orderby('created_at','desc')
+        ->get();
+        return view('admin.index')->with('posts',$posts);
         //return 'Admin';
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.appAdmin')
 
-@section('title','Home')
-
+@section('title','Publicaciones')
+@section('css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@endsection
 @section('content')
-    <h1 class="titulo">Bienvenido administrador</h1>
+
+    <h1 class="titulo">Publicaciones</h1>
+    <a href="publicaciones/create" class="btn btn-success mb-4">Nuevo</a>
+    <div class="container">
+        <div class="row">
+
+            @foreach ($posts as $post)
+                <x-post :post="$post"/>
+            @endforeach
+        </div>
+        
+    </div>
+    
+@endsection
+
+@section('js')
+    <script src="{{asset('js/sweetAlert.js')}}"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @endsection
